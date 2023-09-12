05516cda2ae3261607d17c64356f279fc686a440
Lera <lera1138@yandex.ru>
2023-09-13 01:09:24 +0300

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int initial = 100; // начальный счет
+        int sum = 500; // сумма пополнения
+
+        int bonus;
+        if (sum > 1000) {
+            bonus = sum / 100;
+        } else {
+            bonus = 0;
+        } int result=initial+sum+bonus;
+        System.out.println(bonus);
+        System.out.println(result);
     }
-}
+} //если сумма пополнения 5500,то сумма бонусов 55 и итоговая сумма на счету 5655
+ // если сумма пополнения 500, то  сумма бонусов 0 и итоговая сумма на счету 600
